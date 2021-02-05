5b3733641ccb6185e92c605c06cf6878fd96a9b6
David Moody <DavidRafaMoodyN@gmail.com>
2021-02-04 20:57:11 -0600

    David Moody

---
+++ ProyecFinalPro2/Interfaz/IGestionSerializar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyecFinalPro2.Interfaz
+{
+     interface IGestionSerializar<T>
+     {
+          void Guardar(T obj);
+
+          List<T> Abrir();
+
+          void Modificar();
+
+          void Eliminar();
+     }
+}
---
+++ ProyecFinalPro2/Interfaz/IGestionWPF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyecFinalPro2.Interfaz
+{
+     interface IGestionWPF
+     {
+          void SeputControllers();
+
+          void DragMoveWindows();
+
+          void Exit();
+     }
+}
