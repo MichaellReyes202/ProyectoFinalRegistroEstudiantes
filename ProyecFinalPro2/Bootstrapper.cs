90f2378dba8892d8674f120db4a208ba262077cf
Michaell Joel reyes <michaelljoel.reyes202@gmail.com>
2021-02-09 20:04:34 -0600

    Michaell Reyes

---
+++ ProyecFinalPro2/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using ProyecFinalPro2.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace ProyecFinalPro2
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<UserControl_BD_Student>();
+        }
+
+    }
+}
